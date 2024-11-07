801906dafe8b53703b2493f4678fa69fdc07dfd6
ExtraPolite <imextrapolite2@gmail.com>
2024-11-07 18:18:17 -0500

    input for last name

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,6 +1,9 @@
 export default function General() {
   return <>
-    <label htmlFor="name">First Name:</label>
-    <input type="text" id="name" />
+    <label htmlFor="first-name">First Name:</label>
+    <input type="text" id="first-name" />
+
+    <label htmlFor="last-name">Last Name:</label>
+    <input type="text" id="last-name" />
   </>;
 }
