88bc25d75fbe539c009a7c3e7589112ed77a2578
ExtraPolite <imextrapolite2@gmail.com>
2024-11-08 18:53:06 -0500

    style general section of cv

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -2,21 +2,26 @@ export default function General() {
   return (
     <div className="general">
       <h1>General</h1>
-
-      <label htmlFor="first-name">First Name: </label>
-      <input type="text" id="first-name" />
-
-      <label htmlFor="last-name">Last Name: </label>
-      <input type="text" id="last-name" />
-
-      <label htmlFor="get-email">Email: </label>
-      <input type="email" name="get-email" id="get-email" />
-
-      <label htmlFor="get-phone-number">Phone Number: </label>
-      <input type="number" name="get-phone-number" id="get-phone-number" />
-
-      <label htmlFor="summary">Summary: </label>
-      <textarea name="summary" id="summary"></textarea>
+      <div>
+        <label htmlFor="first-name">First Name: </label>
+        <input type="text" id="first-name" />
+      </div>
+      <div>
+        <label htmlFor="last-name">Last Name: </label>
+        <input type="text" id="last-name" />
+      </div>
+      <div>
+        <label htmlFor="get-email">Email: </label>
+        <input type="email" name="get-email" id="get-email" />
+      </div>
+      <div>
+        <label htmlFor="get-phone-number">Phone Number: </label>
+        <input type="number" name="get-phone-number" id="get-phone-number" />
+      </div>
+      <div>
+        <label htmlFor="summary">Summary: </label>
+        <textarea name="summary" id="summary"></textarea>
+      </div>
     </div>
   );
 }
