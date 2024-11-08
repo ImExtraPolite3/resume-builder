9fbbefe112536343cfdf0f9c268456455467da22
ExtraPolite <imextrapolite2@gmail.com>
2024-11-08 18:25:22 -0500

    make changes to add section tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import General from './components/General';
+
 function App() {
-  return <>
-    <div className="user-input"></div>
-    <div className="user-output"></div>
-  </>;
+  return (
+    <>
+      <section className="user-input">
+        <General />
+      </section>
+      <section className="user-output"></section>
+    </>
+  );
 }
 
 export default App;
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,9 +1,14 @@
 export default function General() {
-  return <>
-    <label htmlFor="first-name">First Name:</label>
-    <input type="text" id="first-name" />
+  return (
+    <div className="general">
+      <label htmlFor="first-name">First Name: </label>
+      <input type="text" id="first-name" />
 
-    <label htmlFor="last-name">Last Name:</label>
-    <input type="text" id="last-name" />
-  </>;
+      <label htmlFor="last-name">Last Name: </label>
+      <input type="text" id="last-name" />
+
+      <label htmlFor="get-email">Email: </label>
+      <input type="email" name="get-email" id="get-email" />
+    </div>
+  );
 }
