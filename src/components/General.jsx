ae015bad701e98308cbb83c044a6c71020e4a1bb
ExtraPolite <imextrapolite2@gmail.com>
2024-11-08 18:30:39 -0500

    add parts for general section

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -9,6 +9,12 @@ export default function General() {
 
       <label htmlFor="get-email">Email: </label>
       <input type="email" name="get-email" id="get-email" />
+
+      <label htmlFor="get-phone-number">Phone Number: </label>
+      <input type="number" name="get-phone-number" id="get-phone-number" />
+
+      <label htmlFor="summary">Summary: </label>
+      <textarea name="summary" id="summary"></textarea>
     </div>
   );
 }
