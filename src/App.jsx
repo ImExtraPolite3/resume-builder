05d5788e15b23c2d97fdd7bc3f6d93eb2665b70f
ExtraPolite <imextrapolite2@gmail.com>
2024-11-09 23:46:06 -0500

    create Education file and add its content. Connect to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import General from './components/General';
+import Education from './components/Education';
 
 function App() {
   return (
     <>
       <section className="user-input">
         <General />
+        <Education />
       </section>
       <section className="user-output"></section>
     </>
