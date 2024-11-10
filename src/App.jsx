b20c9a3fc5aadb9f58516d99bd7db7d4f0df79b7
ExtraPolite <imextrapolite2@gmail.com>
2024-11-10 15:27:44 -0500

    create experience page and connect to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import General from './components/General';
 import Education from './components/Education';
+import Experience from './components/Experience';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <section className="user-input">
         <General />
         <Education />
+        <Experience />
       </section>
       <section className="user-output"></section>
     </>
