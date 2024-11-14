080606fe6d0060b43d84475122928be6e306b7d1
ExtraPolite <imextrapolite2@gmail.com>
2024-11-13 19:10:31 -0500

    change back to normal

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,88 +1,26 @@
-export default function General({ generalInfo, onGeneralInfoChange }) {
-  const handleFirstName = (e) => {
-    onGeneralInfoChange({
-      ...generalInfo,
-      firstName: e.target.value,
-    });
-  };
-
-  const handleLastName = (e) => {
-    onGeneralInfoChange({
-      ...generalInfo,
-      lastName: e.target.value,
-    });
-  };
-
-  const handleEmail = (e) => {
-    onGeneralInfoChange({
-      ...generalInfo,
-      email: e.target.value,
-    });
-  };
-
-  const handlePhoneNumber = (e) => {
-    onGeneralInfoChange({
-      ...generalInfo,
-      phoneNumber: e.target.value,
-    });
-  };
-
-  const handleSummary = (e) => {
-    onGeneralInfoChange({
-      ...generalInfo,
-      summary: e.target.value,
-    });
-  };
-
+export default function General() {
   return (
     <div className="general">
       <h1>General</h1>
       <div>
         <label htmlFor="first-name">First Name: </label>
-        <input
-          type="text"
-          id="first-name"
-          value={generalInfo.firstName}
-          onChange={handleFirstName}
-        />
+        <input type="text" id="first-name" />
       </div>
       <div>
         <label htmlFor="last-name">Last Name: </label>
-        <input
-          type="text"
-          id="last-name"
-          value={generalInfo.lastName}
-          onChange={handleLastName}
-        />
+        <input type="text" id="last-name" />
       </div>
       <div>
         <label htmlFor="get-email">Email: </label>
-        <input
-          type="email"
-          name="get-email"
-          id="get-email"
-          value={generalInfo.email}
-          onChange={handleEmail}
-        />
+        <input type="email" name="get-email" id="get-email" />
       </div>
       <div>
         <label htmlFor="get-phone-number">Phone Number: </label>
-        <input
-          type="number"
-          name="get-phone-number"
-          id="get-phone-number"
-          value={generalInfo.phoneNumber}
-          onChange={handlePhoneNumber}
-        />
+        <input type="number" name="get-phone-number" id="get-phone-number" />
       </div>
       <div>
         <label htmlFor="summary">Summary: </label>
-        <textarea
-          name="summary"
-          id="summary"
-          value={generalInfo.summary}
-          onChange={handleSummary}
-        ></textarea>
+        <textarea name="summary" id="summary"></textarea>
       </div>
     </div>
   );
