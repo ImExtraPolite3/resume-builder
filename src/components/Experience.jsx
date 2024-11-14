7ebd61048fc08d43153a96b2de387464e6778d20
ExtraPolite <imextrapolite2@gmail.com>
2024-11-14 16:50:32 -0500

    dynamically create new experience tabs

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const getIdName = [
   'work-title',
   'company',
@@ -14,29 +16,46 @@ const getName = [
   'End Date:',
 ];
 
-function CreateGeneralContent({ idName, name }) {
+function CreateGeneralContent({ idName, name, key }) {
   return (
     <div>
       <label htmlFor={idName}>{name}</label>
-      <input type="text" id={idName} />
+      <input type="text" key={key} id={idName} />
     </div>
   );
 }
 
 function GeneralContent() {
   return getIdName.map((eachIdName, index) => {
-    return <CreateGeneralContent idName={eachIdName} name={getName[index]} />;
+    return (
+      <CreateGeneralContent
+        idName={eachIdName}
+        key={index}
+        name={getName[index]}
+      />
+    );
   });
 }
 
 export default function Experience() {
+  const [newExperience, setExperiences] = useState([
+    <GeneralContent key={0} />,
+  ]);
+
+  const handleAddExperience = () => {
+    setExperiences((prevExperiences) => [
+      ...prevExperiences,
+      <GeneralContent key={prevExperiences.length} />,
+    ]);
+  };
+
   return (
     <div className="experience">
       <h1>Experience</h1>
-      <button className="create-new-experience">+</button>
-      <div>
-        <GeneralContent />
-      </div>
+      <button className="create-new-experience" onClick={handleAddExperience}>
+        +
+      </button>
+      <div>{newExperience}</div>
     </div>
   );
 }
