4f38e829b8d06c0c62930124ec5f1d16c05f1dbf
ExtraPolite <imextrapolite2@gmail.com>
2024-11-24 18:29:26 -0500

    make sections and connect to App. Add some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import General from './components/General';
 import Education from './components/Education';
 import Experience from './components/Experience';
+import General from './components/General';
 
 function App() {
   return (
     <>
+      <h1>CV Application</h1>
       <section className="user-input">
-        <General />
-        <Education />
-        <Experience />
+        <div className="section-buttons">
+          <button className="show-general">General</button>
+          <button className="show-education">Education</button>
+          <button className="show-experience">Experience</button>
+        </div>
+        <div className="section-input">
+          <General />
+          <Education />
+          <Experience />
+        </div>
       </section>
-      <section className="user-output"></section>
     </>
   );
 }
---
+++ src/components/General.jsx
@@ -0,0 +1,7 @@
+export default function General() {
+  return (
+    <div className="general">
+      <h1>General</h1>
+    </div>
+  );
+}
