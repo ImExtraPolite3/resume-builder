4138988045481863f9daab46d7c733e5ff0541e6
ExtraPolite <imextrapolite2@gmail.com>
2024-11-28 14:28:27 -0500

    add placeholders to general section inputs

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -6,23 +6,37 @@ export default function General({ hideGeneral, onGeneral }) {
     >
       <div>
         <label htmlFor="first-name">First Name: </label>
-        <input type="text" id="first-name" />
+        <input type="text" id="first-name" placeholder="John" />
       </div>
       <div>
         <label htmlFor="last-name">Last Name: </label>
-        <input type="text" id="last-name" />
+        <input type="text" id="last-name" placeholder="Doe" />
       </div>
       <div>
         <label htmlFor="get-email">Email: </label>
-        <input type="email" name="get-email" id="get-email" />
+        <input
+          type="email"
+          name="get-email"
+          id="get-email"
+          placeholder="johndoe@email.com"
+        />
       </div>
       <div>
         <label htmlFor="get-phone-number">Phone Number: </label>
-        <input type="number" name="get-phone-number" id="get-phone-number" />
+        <input
+          type="number"
+          name="get-phone-number"
+          id="get-phone-number"
+          placeholder="123-456-7890"
+        />
       </div>
       <div>
         <label htmlFor="summary">Summary: </label>
-        <textarea name="summary" id="summary"></textarea>
+        <textarea
+          name="summary"
+          id="summary"
+          placeholder="I am very hardworking..."
+        ></textarea>
       </div>
     </div>
   );
