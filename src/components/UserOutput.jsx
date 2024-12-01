c4253aa038b81c199858de3260358d1d87189c87
ExtraPolite <imextrapolite2@gmail.com>
2024-12-01 17:34:44 -0500

    add filler parts for resume text

--- src/components/UserOutput.jsx
+++ src/components/UserOutput.jsx
@@ -2,10 +2,27 @@ export default function UserOutput() {
   return (
     <>
       <div className="resume-general">
-        <h1></h1>
+        <h1>First Name</h1>
+        <h1>Last Name</h1>
+        <p>email</p>
+        <p>phone number</p>
+        <p>summary</p>
       </div>
-      <div className="resume-education"></div>
-      <div className="resume-experience"></div>
+      <div className="resume-education">
+        <p>institution</p>
+        <p>major</p>
+        <p>degree</p>
+        <p>school location</p>
+        <p>start date</p>
+        <p>end date</p>
+      </div>
+      <div className="resume-experience">
+        <p>work title</p>
+        <p>company</p>
+        <p>office location</p>
+        <p>start date</p>
+        <p>end date</p>
+      </div>
     </>
   );
 }
