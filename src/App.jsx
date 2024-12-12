4e87601cd04a6befe4009c11393a5ce344cadd07
ExtraPolite <imextrapolite2@gmail.com>
2024-12-12 13:18:23 -0500

    clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import CreateGeneralInputs from './components/General';
 
 function App() {
-  return <CreateGeneralInputs />;
+  return (
+    <div className="full-page">
+      <div className="user-input">
+        <CreateGeneralInputs />
+      </div>
+      <div className="user-output"></div>
+    </div>
+  );
 }
 
 export default App;
